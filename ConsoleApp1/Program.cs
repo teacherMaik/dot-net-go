0ba62f921e50304752b54108215b59be58ef52bf
Mikelele <d.michaelthomasbennett@gmail.com>
2026-02-17 23:48:47 +0100

    lets lift off

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("This is the First line");
+Console.WriteLine("This is the Second Line");
+Console.WriteLine(420);
+Console.WriteLine(0.25F);
+
+string first_name;
+
+first_name = "Miki";
+
+Console.WriteLine("My name is " + first_name);
+
+var name = "Bob";
+var messages = 3;
+var temp = 34.4;
+
+Console.WriteLine("Hello, " + name + "! You have " + messages + " messages in your inbox. The temperature is " + temp + " celcius");
