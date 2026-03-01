828cb5c99161f99bd4e14832195f337bfcd2008e
Mikelele <d.michaelthomasbennett@gmail.com>
2026-03-01 11:37:24 +0100

    first commit of the day

---
+++ Get started with C# (4)/Program.cs
@@ -0,0 +1,136 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.Metrics;
+
+/*
+Console.WriteLine("Signed integral types:");
+
+Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
+Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
+Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
+Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+
+Console.WriteLine("");
+Console.WriteLine("Unsigned integral types:");
+
+// Unassigned types use bytes for only posoitive values.    
+Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
+Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
+Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
+Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
+
+Console.WriteLine("");
+Console.WriteLine("Floating point types:");
+Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
+Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
+Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+*/
+
+int first = 2;
+string second = "4";
+
+// int sum = first + second; Willl not compile because of type mismatch. We need to convert the string to an int first.
+
+string sum = first + second; // This will compile because of string concatenation. The int will be converted to a string and concatenated with the second string.
+
+/*
+Console.WriteLine(sum); // concatenates to string -> 24
+
+int val = (int)1.5m; // truncates
+Console.WriteLine(val);
+
+int val2 = Convert.ToInt32(1.5m); // rounds up
+Console.WriteLine(val2);
+
+
+string meassurement = "102";
+int result = 0;
+
+if (int.TryParse(meassurement, out result))
+{
+    Console.WriteLine($"Parsed successfully: {result}");
+}
+else
+{
+    Console.WriteLine("Failed to parse the measurement.");
+}
+
+string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+
+decimal sumVals = 0;
+string stringVals = "";
+
+foreach (var value in values)
+{
+    if (decimal.TryParse(value, out decimal parsedVal))
+    {
+        sumVals += parsedVal;
+    }
+    else
+    {
+        stringVals += value;
+    }
+}
+
+Console.WriteLine($"Sum of numeric values: {sumVals}");
+Console.WriteLine($"Non-numeric values: {stringVals}");
+
+string[] palletes = ["B13", "A11", "B11", "A13"];
+Array.Sort(palletes);
+
+Console.WriteLine("After sorting:");
+foreach (var pallete in palletes)
+{
+    Console.WriteLine(pallete);
+}
+
+Console.WriteLine("And reversed");
+Array.Reverse(palletes);
+foreach (var pallete in palletes)
+{
+    Console.WriteLine(pallete);
+}
+
+string str = "abc123";
+char[] strVals = str.ToCharArray();
+
+Array.Reverse(strVals);
+
+string strRev = String.Join(',', strVals);
+
+Console.WriteLine(strRev);
+
+string[] items = strRev.Split(',');
+
+foreach (var item in items)
+{
+    Console.WriteLine(item);
+}
+*/
+
+string message = "Find what is (inside the parentheses)";
+
+int firstParenIndex = message.IndexOf('(');
+int secondParenIndex = message.IndexOf(')');
+
+int length = secondParenIndex - firstParenIndex;
+
+string insideParens = message.Substring(firstParenIndex + 1, length - 1);
+
+Console.WriteLine($"Msg inside parenthesis: \"{insideParens}\"");
+
+
+string newMEssage = "(What if) there are (more than) one (set of parentheses)?";
+while (true)
+{
+    int openingPosition = newMEssage.IndexOf('(');
+    if (openingPosition == -1) break;
+
+    openingPosition += 1;
+    int closingPosition = newMEssage.IndexOf(')');
+    int newLength = closingPosition - openingPosition;
+    Console.WriteLine(newMEssage.Substring(openingPosition, newLength));
+
+    // Note the overload of the Substring to return only the remaining 
+    // unprocessed newMEssage:
+    newMEssage = newMEssage.Substring(closingPosition + 1);
+}
---
+++ challenge-4-1/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Challenge
+{
+    public static void Main()
+    {
+        string pangram = "The quick brown fox jumps over the lazy dog";
+
+        string[] words = pangram.Split(" ");
+        string finalString = "";
+
+        foreach (string word in words)
+        {
+            char[] tempWordChars = word.ToCharArray();
+
+            Array.Reverse(tempWordChars);
+
+            string revTempWord = String.Join("", tempWordChars);
+            finalString += revTempWord + " ";
+        }
+
+        finalString = finalString.TrimEnd();
+
+        Console.WriteLine(finalString);
+
+        // Challenge 2
+        string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+
+        string[] orders = orderStream.Split(",");
+
+        Array.Sort(orders);
+
+        foreach (var order in orders)
+        {
+            if (order.Length != 4)
+            {
+                Console.WriteLine($"{order} \t- not valid");
+            }
+            else
+            {
+                Console.WriteLine(order);
+            }
+        }
+
+    }
+}
+
